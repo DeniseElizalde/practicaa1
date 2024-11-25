dab9398ea6ce35a8556dc23e081fe2b5aaac90d0
DeniseElizalde <denise.elizalde@espoch.edu.ec>
2024-11-25 16:05:00 -0500

    Mi primer commit

---
+++ src/financiero/Financiero.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package financiero;
+
+/**
+ *
+ * @author Dell
+ */
+public class Financiero {
+
+   
+    public static void main(String[] args) {
+        System.out.println("practica 1");
+    }
+    
+}
